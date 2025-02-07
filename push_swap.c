82c4afa4866380f557e18afaf859f52a9302e064
KaraniAbdellah <abdellahkarani@gmail.com>
2025-02-07 18:19:47 +0100

    finich the test algo

--- push_swap.c
+++ push_swap.c
@@ -7,33 +7,25 @@
 int main(int argc, char *argv[]) {
     printf("Welcome To Push Swap Project: \n");
 
+    // The Input Will Be From User To Stack a
     long int *T = check_passing_args(&argc, argv);
     int length = argc - 1;
-    printf("leenght = %d\n", length);
-    for (int i = 0; i < length; i++) {
-        printf("T = %ld\n", T[i]);
+
+    long int n = length;
+    int size_a = n, size_b = 0;
+    Stack *a = malloc(sizeof(Stack) * size_a);
+    Stack *b = malloc(sizeof(Stack) * size_a);
+
+    // Initialize The Stack a
+    for (int i = 0; i < n; i++) {
+        a[i].data = T[i]; b[i].data = 0;
     }
-    printf("size of array T = %ld\n", sizeof(T) / sizeof(long int) + 1);
 
-    // // The Input Will Be From User To Stack a
-    // long int n = sizeof(T);
-    // int size_a = n, size_b = 0;
-    // Stack *a = malloc(sizeof(Stack) * size_a);
-    // Stack *b = malloc(sizeof(Stack) * size_a);
+    display_a_b(a, b, size_a, size_b);
+    sorting_algo(a, b, &size_a, &size_b);
+    display_a_b(a, b, size_a, size_b);
 
-    // // Initialize The Stack a
-    // for (int i = 0; i < n; i++) {
-    //     a[i].data = T[i];
-    // }
-    // for (int i = 0; i < n; i++) {
-    //     b[i].data = 0;
-    // }
-
-    // display_a_b(a, b, size_a, size_b);
-    // sorting_algo(a, b, &size_a, &size_b);
-    // display_a_b(a, b, size_a, size_b);
-
-
+    free(T); free(a); free(b);
 
 
     return 0;
